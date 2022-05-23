707f4a46399d9cd7e0618c1f55921e6a13101f72
Oren Teich <oren@teich.net>
2022-05-23 10:45:48 -0700

    catching up

--- custom_components/iotawatt/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the iotawatt integration."""
-
-DEFAULT_ICON = "mdi:flash"
-DEFAULT_SCAN_INTERVAL = 30
-DOMAIN = "iotawatt"
-COORDINATOR = "coordinator"
-IOTAWATT_API = "iotawatt_api"
-SIGNAL_ADD_DEVICE = "iotawatt_add_device"
-SIGNAL_DELETE_DEVICE = "iotawatt_delete_device"
